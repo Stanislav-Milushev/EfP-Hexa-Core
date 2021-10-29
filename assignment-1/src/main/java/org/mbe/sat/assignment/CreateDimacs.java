01e9c98e99e0b0f39d4428e8706d7a563bf15e36
gr33nte4 <menkata@gmx.de>
2021-10-29 16:21:28 +0200

    Revert "Dimacs file creation for crossTreeCNF"
    This reverts commit 1c12e66ec1e0d5c34ea2a561d89709920319e77a.

--- assignment-1/src/main/java/org/mbe/sat/assignment/CreateDimacs.java
+++ assignment-1/src/main/java/org/mbe/sat/assignment/CreateDimacs.java
@@ -1,40 +1,8 @@
 package org.mbe.sat.assignment;
 
-import java.io.*;
-
-import org.logicng.formulas.Formula;
-import org.logicng.formulas.FormulaFactory;
-import org.logicng.io.parsers.ParserException;
-import org.logicng.io.parsers.PropositionalParser;
-import org.logicng.io.writers.FormulaDimacsFileWriter;
-
 public class CreateDimacs {
-		String fileName1;
-		Formula formula1 ;
-	public CreateDimacs(String fileName,String formula) throws ParserException {
-		this.formula1 = Formulagenerator(formula);
-		this.formula1=formula1.cnf();
-		this.fileName1 = fileName;
-    }
-	public void WriteDimacs() {
-		try {
-			FormulaDimacsFileWriter.write("src/main/resources/"+fileName1, formula1, false);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	private Formula Formulagenerator(String formula){
-		FormulaFactory f=new FormulaFactory();
-		PropositionalParser p=new PropositionalParser(f);
-		
-		try {
-			return p.parse(formula);
-		} catch (ParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-	}
+    /**
+     * TODO:
+     *  Create a Dimacs-File from FeatureTreeCNF and FeatureCrossTreeCNF
+     */
 }
