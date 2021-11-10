b75bb616cf8b8976dd74b05360f60b1622b3737b
Felix <felixschon@web.de>
2021-11-10 16:23:45 +0100

    Update Simplifyer

--- assignment-2/src/main/java/org/mbe/sat/assignment/procedure/Simplifier.java
+++ assignment-2/src/main/java/org/mbe/sat/assignment/procedure/Simplifier.java
@@ -1,8 +1,16 @@
 package org.mbe.sat.assignment.procedure;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.mbe.sat.api.procedure.ISimplifier;
 import org.mbe.sat.core.model.Assignment;
+import org.mbe.sat.core.model.formula.Atom;
 import org.mbe.sat.core.model.formula.CnfFormula;
+import org.mbe.sat.core.model.formula.Literal;
+import org.mbe.sat.core.model.formula.Literal.Polarity;
+import org.mbe.sat.core.model.formula.Or;
+import org.mbe.sat.core.model.formula.Tristate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +26,55 @@ public class Simplifier implements ISimplifier<CnfFormula, Assignment> {
 
         CnfFormula evaluatedFormula = cnfFormula.evaluate(assignment);
         // TODO: Simplify evaluated formula
-
+        
+        Set<Or<Atom>> allClauses = evaluatedFormula.getClauses(); // alle klauseln holen
+        Iterator<Or<Atom>> clauseIterator = allClauses.iterator(); // iterator erstellen
+        
+        while(clauseIterator.hasNext()) { // für jede Klausel...
+        	
+        	Or<Atom> currentClause = clauseIterator.next(); // die aktuelle klausel
+        	Set<Literal> literals = currentClause.getLiterals(); // die literale der aktuellen klausel
+        	Iterator<Literal> literalIterator = literals.iterator();
+        	
+        	while(literalIterator.hasNext()) { // für jedes literal in der aktuellen klausel
+        		
+        		Literal currentLiteral = literalIterator.next(); // aktuelles literal in der aktuelle klausel
+        		Polarity polarity = currentLiteral.getPolarity(); // polarität des literals
+        		Tristate belegung = currentLiteral.isSatisfiedWith(assignment); // belegung des literals
+        		
+        		switch(belegung) { // Fallunterscheidung der Belegung => FALSE, TRUE oder UNDEFINED
+        			case FALSE: {
+        				if(polarity.isPositive()) { // Polaritaet ist positiv, Belegung ist false
+        					currentClause.getLiterals().remove(currentLiteral); // entfernen des akutellen literals aus der liste
+        				}else { // Polaritaet ist negativ, Belegung ist false
+        					allClauses.remove(currentClause); // entfernen der klausel
+        				}
+        				break;
+        			}
+        				
+        			case TRUE: {
+        				if(polarity.isPositive()) { // Polaritaet ist positiv, Belegung ist true
+        					allClauses.remove(currentClause); // entfernen der klausel
+        				}else { // Polaritaet ist negativ, Belegung ist true
+        					currentClause.getLiterals().remove(currentLiteral); // entfernen der klausel
+        				}
+        				break;
+        			}
+        				
+        			case UNDEFINED: { // belegung ist undefined
+        				break; // nichts tun
+        			}	
+        		}
+        		
+        		if(currentClause.getLiterals().isEmpty()) {
+        			return null; // unerfuellbar weil die aktuelle klausel leer ist
+        		}
+        		
+        		        		
+        	}
+        	
+        }
+        
         return evaluatedFormula;
     }
 }
