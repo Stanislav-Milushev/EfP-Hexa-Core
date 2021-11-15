360ed77a8957af3fc8878dd3a2954eaa143a3d0d
Daniel Karl <daniel.karl@student.uni-siegen.de>
2021-11-15 20:36:46 +0100

    Updated Simplifier

--- assignment-2/src/main/java/org/mbe/sat/assignment/procedure/Simplifier.java
+++ assignment-2/src/main/java/org/mbe/sat/assignment/procedure/Simplifier.java
@@ -2,13 +2,10 @@ package org.mbe.sat.assignment.procedure;
 
 import java.util.Iterator;
 import java.util.Set;
-
 import org.mbe.sat.api.procedure.ISimplifier;
 import org.mbe.sat.core.model.Assignment;
 import org.mbe.sat.core.model.formula.Atom;
 import org.mbe.sat.core.model.formula.CnfFormula;
-import org.mbe.sat.core.model.formula.Literal;
-import org.mbe.sat.core.model.formula.Literal.Polarity;
 import org.mbe.sat.core.model.formula.Or;
 import org.mbe.sat.core.model.formula.Tristate;
 import org.slf4j.Logger;
@@ -23,58 +20,38 @@ public class Simplifier implements ISimplifier<CnfFormula, Assignment> {
 
     public CnfFormula simplify(CnfFormula cnfFormula, Assignment assignment) {
         LOG.debug("Simplifying formula '{}' with assignment '{}'", cnfFormula, assignment);
-
-        CnfFormula evaluatedFormula = cnfFormula.evaluate(assignment);
-        // TODO: Simplify evaluated formula
+    	
+        //CNF-Formel wird evaluiert
+    	CnfFormula evaluatedFormula = cnfFormula.evaluate(assignment);
+        Set<Or<Atom>> clauses = evaluatedFormula.getClauses();	//hole Klauseln der evaluierten CNF
+        Iterator<Or<Atom>> clausesIterator = clauses.iterator();
         
-        Set<Or<Atom>> allClauses = evaluatedFormula.getClauses(); // alle klauseln holen
-        Iterator<Or<Atom>> clauseIterator = allClauses.iterator(); // iterator erstellen
-        
-        while(clauseIterator.hasNext()) { // für jede Klausel...
+        /*Durch alle Klauseln der Formel iterieren*/
+        while(clausesIterator.hasNext()) {
         	
-        	Or<Atom> currentClause = clauseIterator.next(); // die aktuelle klausel
-        	Set<Literal> literals = currentClause.getLiterals(); // die literale der aktuellen klausel
-        	Iterator<Literal> literalIterator = literals.iterator();
+        	Or<Atom> currentClause = clausesIterator.next(); //aktuelle Klausel
+        	Set<Atom> literals = currentClause.getOperands(); //alle Literale der aktuellen Klausel
+        	Iterator<Atom> literalIterator = literals.iterator();
+        	boolean hasTrue = false; // zum ueberpruefen, ob in der Klausel ein Literal auf Top gesetzt wurde
         	
-        	while(literalIterator.hasNext()) { // für jedes literal in der aktuellen klausel
+        	
+        	/*Durch alle Literale der Klausel iterieren*/
+        	while(literalIterator.hasNext()) {
         		
-        		Literal currentLiteral = literalIterator.next(); // aktuelles literal in der aktuelle klausel
-        		Polarity polarity = currentLiteral.getPolarity(); // polarität des literals
-        		Tristate belegung = currentLiteral.isSatisfiedWith(assignment); // belegung des literals
+        		Atom currentliteral = literalIterator.next(); // aktuelles Literal
+        		Tristate belegung = currentliteral.isSatisfiedWith(assignment); // Belegung des aktuellen Literals
         		
-        		switch(belegung) { // Fallunterscheidung der Belegung => FALSE, TRUE oder UNDEFINED
-        			case FALSE: {
-        				if(polarity.isPositive()) { // Polaritaet ist positiv, Belegung ist false
-        					currentClause.getLiterals().remove(currentLiteral); // entfernen des akutellen literals aus der liste
-        				}else { // Polaritaet ist negativ, Belegung ist false
-        					allClauses.remove(currentClause); // entfernen der klausel
-        				}
-        				break;
-        			}
-        				
-        			case TRUE: {
-        				if(polarity.isPositive()) { // Polaritaet ist positiv, Belegung ist true
-        					allClauses.remove(currentClause); // entfernen der klausel
-        				}else { // Polaritaet ist negativ, Belegung ist true
-        					currentClause.getLiterals().remove(currentLiteral); // entfernen der klausel
-        				}
-        				break;
-        			}
-        				
-        			case UNDEFINED: { // belegung ist undefined
-        				break; // nichts tun
-        			}	
+        		
+        		/*Wenn die Belegung Top ist dann wird hasTrue auf true gesetzt (=>wenn hasTrue = true, dann Klausel entfernen)*/
+        		 if(belegung == Tristate.TRUE) {
+        			hasTrue = true;
         		}
-        		
-        		if(currentClause.getLiterals().isEmpty()) {
-        			return null; // unerfuellbar weil die aktuelle klausel leer ist
-        		}
-        		
-        		        		
         	}
-        	
+        	/*entferne Klausel, wenn ein Top Literal enthalten ist*/
+        	if(hasTrue) {
+        		clausesIterator.remove();
+        	}
         }
-        
-        return evaluatedFormula;
+        return evaluatedFormula;   
     }
 }
